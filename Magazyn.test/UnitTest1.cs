68fb53f8c61c83b5412d68d9040f787bb60c757f
Bartek Garus <bartekk.garus@gmail.com>
2022-05-15 15:43:38 +0200

    unit test

---
+++ Magazyn.test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace Magazyn.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int i = 0;
+            i++;
+
+            Assert.Equal(1, i);
+        }
+    }
+}
