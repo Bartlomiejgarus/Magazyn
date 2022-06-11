5e3983e079ab032ce795864e57c802306cf465bf
KarolBochenek <95887356+KarolBochenek@users.noreply.github.com>
2022-06-11 21:32:08 +0200

    Revert "Revert "Dodanie struktury użytkowników""
    This reverts commit 3d3b087062cc91e41836948b17ba7019fa028290.

---
+++ Magazyn/Models/Dictionares/StatusModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Models.Dictionares
+{
+    public class StatusModel : EntityModel
+    {
+        public string Value { get; set; }
+    }
+}
